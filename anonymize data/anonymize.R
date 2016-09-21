b8c6937bf79da8fa454b27f82b78974cda6ed7bc
mcooperci <mw.coop.r@gmail.com>
2016-09-21 16:52:29 -0400

    Added database anonymizing function

---
+++ anonymize data/anonymize.R
@@ -0,0 +1,63 @@
+#' First dump a fresh copy of the database into staging.  
+#' 
+#' Then run the following code on staging.
+#' 
+#' Finally, you can dump a copy of the staging db, so have a portable, pii-free
+#' copy of the database
+#'
+#
+
+library(DBI)
+library(dplyr)
+
+setwd('D:/Documents and Settings/mcooper/GitHub/vs-data-tools/anonymize data')
+
+source('D://staging_connection.R')
+
+vs_db <- src_postgres(dbname=staging_dbname, host=staging_host,
+                      user=staging_user, password=staging_password,
+                      port=staging_port)
+
+#First drop audit and users_schema
+#Then drop household_secV
+
+dbSendQuery(vs_db$con, sql('DROP TABLE "household_secV" CASCADE;'))
+dbSendQuery(vs_db$con, sql('DROP SCHEMA audit CASCADE;'))
+dbSendQuery(vs_db$con, sql('DROP SCHEMA users_schema CASCADE;'))
+dbSendQuery(vs_db$con, sql('DROP TRIGGER farmfieldsoils_yields_paddy_maize_hh_ref ON farmfieldsoils_yields_paddy_maize;'))
+dbSendQuery(vs_db$con, sql('DROP TRIGGER farmfieldsoils_yields_maize_hh_ref ON farmfieldsoils_yields_maize;'))
+dbSendQuery(vs_db$con, sql('DROP TRIGGER farmfieldsoils_yields_weight_hh_ref ON farmfieldsoils_yields_weight;'))
+dbSendQuery(vs_db$con, sql('DROP TRIGGER farmfieldsoils_lab_hh_ref ON farmfieldsoils_lab;'))
+dbSendQuery(vs_db$con, sql('DROP TRIGGER farmfieldsoils_hh_ref ON farmfieldsoils;'))
+dbSendQuery(vs_db$con, sql('DROP TRIGGER farmfieldsoils_processed_hh_ref ON farmfieldsoils_processed;'))
+dbSendQuery(vs_db$con, sql('DROP TRIGGER agric_hh_ref ON agric;'))
+dbSendQuery(vs_db$con, sql('DROP TRIGGER household_hh_ref ON household;'))
+
+pii <- read.csv('pii fields.csv', stringsAsFactors=F)
+
+for (i in 1:nrow(pii)){
+  if (pii$action[i]=='removed'){
+    dbSendQuery(vs_db$con, sql(paste0('UPDATE "', pii$table[i], '" SET ', pii$var[i], ' = NULL')))
+  }else if(pii$action[i]=='overwrite'){
+    dbSendQuery(vs_db$con, sql(paste0('UPDATE "', pii$table[i], '" SET ', pii$var[i], " = concat('dummy data ', substr(md5(random()::text), 1, 5))")))
+  }else if(pii$action[i]=='randomized'){
+    cty_lscp <- tbl(vs_db, pii$table[i]) %>% 
+      select(country, landscape_no) %>% 
+      distinct() %>% 
+      data.frame()
+    for (j in 1:nrow(cty_lscp)){
+      eplots <- tbl(vs_db, pii$table[i]) %>% 
+        filter(country == cty_lscp$country[j], landscape_no == cty_lscp$landscape_no[j]) %>% 
+        select(eplot_no) %>% 
+        distinct() %>%
+        data.frame()
+      eplots$new <- sample(eplots$eplot_no, nrow(eplots), F)
+      for (k in 1:nrow(eplots)){
+        dbSendQuery(vs_db$con, sql(paste0('UPDATE "', pii$table[i], '" SET eplot_no = \'', eplots$new[k],
+                                          "' WHERE country = '", cty_lscp$country[j], "' AND landscape_no = '",
+                                          cty_lscp$landscape_no[j], "' AND eplot_no = '", eplots$eplot_no[k], "';")))
+      }
+    }
+  }
+}
+
