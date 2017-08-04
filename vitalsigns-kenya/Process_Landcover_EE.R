0a6e41709844ee8317cd5cc08cf3cc5763319c1d
mcooperci <mw.coop.r@gmail.com>
2017-08-04 12:53:33 -0700

    process landcover

--- vitalsigns-kenya/Process_Landcover_EE.R
+++ vitalsigns-kenya/Process_Landcover_EE.R
@@ -1,89 +1,91 @@
-setwd('D://Documents and Settings/mcooper/GitHub/vs-data-tools/vitalsigns-kenya/')
+setwd('D://Documents and Settings/mcooper/Documents/VS Kenya Data Processing/')
 
-library(tidyr)
+library(rjson)
 library(dplyr)
+library(tidyr)
+library(reshape2)
 
-dat <- read.csv('ee_output.csv')
+getVal <- function(substr){
+  spl <- strsplit(substr, '=')[[1]][2] %>%
+    as.numeric
+  return(spl)
+}
 
-dat[,grepl('X', names(dat))][is.na(dat[,grepl('X', names(dat))])] <- 0
-
-###################################
-##PAs
-###################################
-
-PA <- dat %>% filter(dataset=="PA")
-
-PA$sum <- rowSums(PA[ , c("X0.0", "X1.0")], na.rm=T)
-
-PA$Percent_Unprotected <- PA$X1.0/PA$sum
-PA$Percent_Protected <- PA$X0.0/PA$sum
-
-PA$Percent_Unprotected[is.nan(PA$Percent_Unprotected)] <- 0
-PA$Percent_Protected[is.nan(PA$Percent_Protected)] <- 0
-
-PA <- PA %>% dplyr::select(Percent_Unprotected, Percent_Protected, type, id) %>%
-  gather(category, value, -type, -id)
+getDf <- function(str){
+  cats <- gsub('\\{|\\}', '', str) %>%
+    strsplit(', ') %>%
+    .[[1]]
   
-#####################################
-#Land Cover
-######################################
+  Forest <- 0
+  Grass <- 0
+  Cropland <- 0
+  Water <- 0
+  Null <- 0
+  for (i in cats){
+    if (substr(i, 1, 3)=='1.0'){
+      Forest <- getVal(i)
+    }else if(substr(i, 1, 3)=='4.0'){
+      Grass <- getVal(i)
+    }else if (substr(i, 1, 3)=='6.0'){
+      Cropland <- getVal(i)
+    }else if (substr(i, 1, 3)=='9.0'){
+      Water <- getVal(i)
+    }else if (substr(i, 1, 3)=='0.0'){
+      Null <- getVal(i)
+    }else{
+      stop('Missing category in ', i)
+    }
+  }
+  DF <- data.frame(Forest=Forest, Grass=Grass, 
+                   Cropland=Cropland, Water=Water,
+                   Null=Null)
+  return(DF)
+}
 
-LC <- dat %>% filter(grepl('cci', dataset))
+years <- c('2014', '2010', '2005', '2000', '1995', '1990')
 
-zcols <- apply(LC, MARGIN = 2, FUN = function(x){sum(as.numeric(x))})==0
+accum <- data.frame()
+for (y in years){
+  print(y)
+  s <- read.csv(paste0('S', y, '.csv')) %>%
+    select(histogram, id, name, type)
+                  
+  s$histogram <- as.character(s$histogram)
+  s$date <- y
 
-zcols[is.na(zcols)] <- FALSE
+  t <- lapply(X=s$histogram, FUN = getDf)
+  catsdf <- Reduce(f=bind_rows, t)
+  
+  s$histogram <- NULL
+  
+  s <- cbind(s, catsdf)
+  
+  accum <- bind_rows(accum, s)
+}
 
-LC <- LC[ , !zcols]
+write.csv(accum, 'AllLandcover.csv', row.names=F)
 
-LC$sum <- rowSums(LC[ , grepl('X', names(LC))])
+accum$total <- rowSums(accum[ , c('Forest', 'Grass', 'Cropland', 'Water')], na.rm=T)
+  
+accum$Grass <- accum$Grass/accum$total
+accum$Forest <- accum$Forest/accum$total
+accum$Cropland <- accum$Cropland/accum$total
+accum$Water <- accum$Water/accum$total  
 
-ag <- paste0('X', c('10', '11', '20')) 
-ag_mosaic <- paste0('X', c('30', '40' ))
-water <- paste0('X', c('210'))
-forest <- paste0('X', c('100', '160', '170', '50', '60', '61', '62', '70', '90'))
-herbs_shrubs_grass <- paste0('X', c('110', '120', '122', '180', '130'))
-sparse_veg_bare <- paste0('X', c('150', '153', '152', '200', '201', '202')) 
-urban <- paste0('X', c('190'))
 
-LC$pct_ag <- rowSums(LC[ , ag], na.rm=T)/LC$sum
-LC$pct_ag_mosaic <- rowSums(LC[ , ag_mosaic], na.rm=T)/LC$sum
-LC$pct_water <- LC[ , water]/LC$sum
-LC$pct_forest <- rowSums(LC[ , forest], na.rm=T)/LC$sum
-LC$pct_herbs_shrubs_grass <- rowSums(LC[ , herbs_shrubs_grass], na.rm=T)/LC$sum
-LC$pct_sparse_veg_bare <- rowSums(LC[ , sparse_veg_bare], na.rm=T)/LC$sum
-LC$pct_urban <- LC[ , urban]/LC$sum
+accum$Null <- NULL
+accum$total <- NULL
+accum$name <- NULL
 
-LC <- LC %>% dplyr::select(dataset, id, type, pct_ag, pct_ag_mosaic, 
-              pct_forest, pct_herbs_shrubs_grass, pct_sparse_veg_bare,
-              pct_water) %>% 
-  gather(category, value, -dataset, -id, -type) %>%
-  mutate(category=paste0(category, '_20', substr(dataset, 4,5))) %>%
-  dplyr::select(-dataset)
+accum %>%
+  melt(id.vars=c('id', 'type', 'date'))
 
-pop <- dat %>% 
-  filter(dataset=='population') %>%
-  dplyr::select(value=sum, id, type) %>%
-  mutate(category='Estimated Total Pop 2015')
+accum$pa_id[accum$type=='pa'] <- accum$id[accum$type=='pa']
+accum$waterb_id[accum$type=='waterb'] <- accum$id[accum$type=='waterb']
+accum$county_id[accum$type=='county'] <- accum$id[accum$type=='county']
+accum$country_id[accum$type=='country'] <- accum$id[accum$type=='country']
 
-mkt <- dat %>% 
-  filter(dataset=='market') %>%
-  dplyr::select(value=mean, id, type) %>%
-  mutate(category='Mean Travel Time to City with Pop > 50k')
-         
-final <- Reduce(f=bind_rows, x = list(mkt, pop, LC, PA))
 
-final$pa_id <- final$id
-final$county_id <- final$id
-final$waterb_id <- final$id
-final$country_id <- final$id
 
-final$pa_id[final$type != 'pa'] <- NA
-final$county_id[final$type != 'county'] <- NA
-final$waterb_id[final$type != 'waterb'] <- NA
-final$country_id[final$type != 'country'] <- NA
-
-final <- final %>% dplyr::select(-type)
-
-write.csv(final, 'indicators_landcover.csv', row.names=F)
+  
   
